286428383b750516b064b67f0dc6e5cd0780a33f
Yeum Gu Whan <guwhan.yeum@flitto.com>
2021-12-08 02:08:09 +0900

    feat: lecture 1 기본 알고리즘 학습

--- inflearn/lecture_1/8_일곱난쟁이.js
+++ inflearn/lecture_1/8_일곱난쟁이.js
@@ -19,8 +19,19 @@
 20 7 23 19 10 8 13
  */
 function solution (arr) {
-  let answer
-  return answer;
+  const answer = [...arr]
+  let totalSum = arr.reduce((sum, v) => sum + v, 0)
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if ((totalSum - arr[i] - arr[j]) === 100) {
+        delete answer[i]
+        delete answer[j]
+        break
+      }
+    }
+  }
+
+  return answer.filter(v => v);
 }
 
 let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13]
