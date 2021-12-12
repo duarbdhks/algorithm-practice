d9a6da684998c8b804013984126903d0268db614
Yeum Gu Whan <guwhan.yeum@flitto.com>
2021-12-12 23:29:46 +0900

    feat: stack 및 queue 시작

---
+++ inflearn/lecture_6/5_쇠막대기.js
@@ -0,0 +1,7 @@
+function solution (s) {
+  let answer = 0;
+  return answer;
+}
+
+let a = '()(((()())(())()))(())'
+console.log(solution(a))
---
+++ inflearn/lecture_6/6_공주구하기.js
@@ -0,0 +1,6 @@
+function solution (n, k) {
+  let answer;
+  return answer;
+}
+
+console.log(solution(8, 3))
